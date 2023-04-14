de3cb44203daba4fd07fd0f4eeded16dc02aef39
sumon-hossain <sumonengr.96@gmail.com>
2023-04-15 01:22:00 +0600

    WIP

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,8 @@
-let users = [];
-export const getAllUser = async (req, res, next) => {
-  res.json(users);
-};
 
 export const postUser = async (req, res, next) => {
   const { contact_id, first_name,full_name, email } = req.body;
 
   const newUser = { contact_id, first_name,full_name, email };
-  users.push(newUser);
+  
   res.json(newUser);
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { getAllUser, postUser } from "../controllers/userController.js";
+import { postUser } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/", getAllUser);
 userRouter.post("/user", postUser);
 
 export default userRouter;
